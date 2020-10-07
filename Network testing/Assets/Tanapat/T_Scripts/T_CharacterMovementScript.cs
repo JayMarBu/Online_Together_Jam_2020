86e04424263af519c2585f89b0c5f32fb8ac2706
TaniCorn <63819551+TaniCorn@users.noreply.github.com>
2020-10-07 16:35:52 +0100

    My own Test Zone
    I've just made a simple character movement for now, i'll be doing more later

---
+++ Network testing/Assets/Tanapat/T_Scripts/T_CharacterMovementScript.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class T_CharacterMovementScript : MonoBehaviour
+{
+
+    #region privateVariables
+    [Header("PlayerComponents")]
+    private Rigidbody playerRigidBody;
+    private CharacterController playerCharacterController;
+    [Space]
+
+    [Header("Uncategorised")]
+    private Vector3 playerMovement;
+    public Vector3 playerFallingVelocity;
+    [Space]
+
+    #endregion
+    #region publicVariables
+    [Header("PlayerProperties")]
+    [Range(1, 100)]
+    public int playerSpeed = 15;
+    [Range(-1, -100)]
+    public float playerGravity = -10;
+
+    #endregion
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerRigidBody = GetComponentInChildren<Rigidbody>();
+        playerCharacterController = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        playerFallingVelocity.y += playerGravity * Time.deltaTime;
+
+        playerMovement = new Vector3
+            (Input.GetAxisRaw("Horizontal") * playerSpeed * Time.deltaTime, playerFallingVelocity.y * Time.deltaTime,
+            Input.GetAxisRaw("Vertical") * playerSpeed * Time.deltaTime);
+
+
+        playerCharacterController.Move(playerMovement);
+        //playerCharacterController.Move();
+
+    }
+}
