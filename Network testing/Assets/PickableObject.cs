6e0e5a61a43540a6dec70fe0af968deb707f3929
TaniCorn <63819551+TaniCorn@users.noreply.github.com>
2020-10-12 14:42:08 +0100

    PlayerMovement
    The Player Movement is completed, currently working on picking up objects

---
+++ Network testing/Assets/PickableObject.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickableObject : MonoBehaviour
+{
+    [Header("PlayerHand")]
+    public Transform playerHandLocation;
+
+    [Header("PlayerInteractableDistance")]
+    public Transform objectOrigin;
+    public int interactDistance;
+    public LayerMask playerMask;
+    public bool canBePickedUp;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        canBePickedUp = Physics.CheckSphere(objectOrigin.position, interactDistance , playerMask);
+    }
+
+    void Pickup()
+    {
+        if (canBePickedUp)
+        {
+            gameObject.transform.position = playerHandLocation.position;
+        }
+    }
+}
--- Network testing/Assets/Tanapat/T_Scripts/T_CharacterMovementScript.cs
+++ Network testing/Assets/Tanapat/T_Scripts/T_CharacterMovementScript.cs
@@ -7,26 +7,48 @@ public class T_CharacterMovementScript : MonoBehaviour
 
     #region privateVariables
     [Header("PlayerComponents")]
+
     private Rigidbody playerRigidBody;
     private CharacterController playerCharacterController;
     [Space]
 
+    [Space]
+
     [Header("Uncategorised")]
+
     private Vector3 playerMovement;
     public Vector3 playerFallingVelocity;
     [Space]
 
     #endregion
     #region publicVariables
+    [Header("PlayerReferenced")]
+    public Transform playerHand;
+    [Space]
+
     [Header("PlayerProperties")]
     [Range(1, 100)]
     public int playerSpeed = 15;
     [Range(-1, -100)]
     public float playerGravity = -10;
+    [Range(1, 100)]
+    public float playerJumpHeight = 2;
+    [Space]
+
+    [Header("PlayerHand")]
+    public bool playerHandEmpty;
+    [Space]
+
+    [Header("PlayerGroundChecks")]
+    public Transform playerGroundCheck;
+    public float groundDistance = 0.4f;
+    public LayerMask groundMask;
+    public bool isGrounded;
+
 
     #endregion
 
-    // Start is called before the first frame update
+    // Grab components
     void Start()
     {
         playerRigidBody = GetComponentInChildren<Rigidbody>();
@@ -36,15 +58,46 @@ public class T_CharacterMovementScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        #region FallingJump
+        //Projects a sphere underneath player to check ground layer
+        isGrounded = Physics.CheckSphere(playerGroundCheck.position, groundDistance, groundMask);
+        
+        //Player recieves a constant y velocity from gravity
         playerFallingVelocity.y += playerGravity * Time.deltaTime;
 
+        if(isGrounded && playerFallingVelocity.y < 0)
+        {
+            playerFallingVelocity.y = -1f;
+        }
+
+        //Player can jump
+        if (Input.GetButtonDown("Jump") && isGrounded)
+        {
+            playerFallingVelocity.y = Mathf.Sqrt(playerJumpHeight * -2 * playerGravity);
+        }
+        #endregion
+
+        #region PlayerMovement
+        //Complete player movement
         playerMovement = new Vector3
             (Input.GetAxisRaw("Horizontal") * playerSpeed * Time.deltaTime, playerFallingVelocity.y * Time.deltaTime,
             Input.GetAxisRaw("Vertical") * playerSpeed * Time.deltaTime);
-
+        
 
         playerCharacterController.Move(playerMovement);
-        //playerCharacterController.Move();
+        #endregion
 
+        if(Input.GetKey("E") && playerHandEmpty)
+        {
+            //pickup object
+            //Check if object is in range
+            //grab object.transform in range
+            //keep object.transform on player hand
+            Invoke("Pickup", 1);
+        }
+        else if(Input.GetKey("E"))
+        {
+            //Drop Object or place object
+        }
     }
 }
