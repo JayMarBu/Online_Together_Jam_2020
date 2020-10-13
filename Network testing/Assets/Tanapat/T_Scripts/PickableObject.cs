d0b947f271cfb4f00b000671cb9b516775b42424
TaniCorn <63819551+TaniCorn@users.noreply.github.com>
2020-10-13 19:23:16 +0100

    Moved components from template character to peanut character

---
+++ Network testing/Assets/Tanapat/T_Scripts/PickableObject.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickableObject : MonoBehaviour
+{
+
+    public Transform playerHandLocation;
+    public bool playerHandEmpty;
+    public bool inRange;
+
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (inRange)
+        {
+            playerHandLocation = GameObject.Find("Item").transform;
+
+            if (Input.GetKeyDown(KeyCode.E) && FindObjectOfType<T_CharacterMovementScript>().playerHandEmpty == true)
+            {
+
+                GetComponent<Rigidbody>().velocity = new Vector3(0,0,0);
+                //Can't figure out how to stop rotation, however it could be a feature, so i've always added rotation
+                GetComponent<Rigidbody>().AddTorque(10, 10, 0);
+
+                GetComponent<Rigidbody>().useGravity = false;
+                GetComponent<BoxCollider>().enabled = false;
+
+                this.transform.position = playerHandLocation.position;
+                this.transform.parent = GameObject.Find("Item").transform;
+
+                Debug.Log("Pickup");
+                FindObjectOfType<T_CharacterMovementScript>().playerHandEmpty = false;
+
+                
+            }
+            else if (Input.GetKeyDown(KeyCode.E))
+            {
+                GetComponent<Rigidbody>().useGravity = true;
+                GetComponent<BoxCollider>().enabled = true;
+
+                this.transform.parent = null;
+                FindObjectOfType<T_CharacterMovementScript>().playerHandEmpty = true;
+
+                Debug.Log("Drop");
+            }
+        }
+    }
+
+
+    #region isPlayerInRange
+    public void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            Debug.Log("Enter");
+            inRange = true;
+        }
+
+    }
+    public void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            Debug.Log("Exit");
+            inRange = false;
+        }
+    }
+    #endregion
+}
